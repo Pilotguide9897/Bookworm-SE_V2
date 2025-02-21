434ed4842f0570a9666e55b311d0282367ea0f8b
Jared Green <jaredryan1997@hotmail.com>
2025-02-21 10:20:25 -0400

    working in local environment

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose
+  .connect(
+    "mongodb+srv://Pilotguide9897:JaRyGn97@cluster0.es2k8ab.mongodb.net/BookwormSE?retryWrites=true&w=majority&appName=Cluster0" ||
+      "mongodb://127.0.0.1:27017/googlebooks"
+  )
+  .then(() => {
+    console.info("Connected to the DB");
+  })
+  .catch((e) => {
+    console.log("Error:", e);
+  });
 
 module.exports = mongoose.connection;
